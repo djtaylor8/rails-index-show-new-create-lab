61a5ed6527200cdc2740676bf50e8db9d615e929
DJ Taylor <dtaylor88@gmail.com>
2021-07-27 16:43:05 -0700

    commit tree 36253b0c050176b1f2624e7c306304a3c5ee49f6

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+coupon_list = [
+    [ "code1", "store1" ],
+    [ "code2", "store2" ],
+    [ "code3", "store3" ],
+    [ "code4", "store4" ]
+  ]
+  
+  coupon_list.each do |code, store|
+    Coupon.create( coupon_code: code, store: store )
+  end
